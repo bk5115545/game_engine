e7b0610737fcfe029f935767b24c944ea66ff5f5
Casey Megginson <tenncasey@gmail.com>
2015-12-17 18:51:22 -0600

    Fixed a lot of compiler errors related to 2D rendering, but application now currently fails to link due to issues related to Box2D.

--- include/components/ComponentFactories.h
+++ include/components/ComponentFactories.h
@@ -1,37 +1,38 @@
-// #pragma once
-//
-// #include <memory>
-// #include <string>
-//
-// #include "Box2D/Box2D.h"
-// #include "core/PugiXML/pugixml.hpp"
-//
-// #include "art_library/ArtAssetLibrary.h"
-//
-// #include "components/Bullet.h"
-// #include "components/Carrier.h"
-// #include "components/Infantry.h"
-// #include "components/Player.h"
-//
-// #include "render/sdl/sdl_animation.h"
-//
-// #include "render/opengl/gl_drawable.h"
-//
-// #include "core/input_device.h"
-// #include "render/sdl/sprite.h"
-// #include "core/physics_2d.h"
-//
-// #include "virtual/component.h"
-//
-// class ComponentFactory {
-// protected:
-//
-// public:
-//     ComponentFactory();
-//     virtual ~ComponentFactory() = 0;
-//     virtual Component* Create(Actor*, pugi::xml_node) = 0;
-// };
-//
+#pragma once
+
+#include <memory>
+#include <string>
+
+#include "Box2D/Box2D.h"
+#include "core/PugiXML/pugixml.hpp"
+
+#include "art_library/ArtAssetLibrary.h"
+
+#include "components/Bullet.h"
+#include "components/Carrier.h"
+#include "components/Infantry.h"
+#include "components/Player.h"
+
+#include "render/sdl/sdl_animation.h"
+#include "render/sdl/sdl_renderer.h"
+
+#include "render/opengl/gl_drawable.h"
+
+#include "core/input_device.h"
+#include "render/sdl/sprite.h"
+#include "core/physics_2d.h"
+
+#include "virtual/component.h"
+
+class ComponentFactory {
+protected:
+
+public:
+    ComponentFactory();
+    virtual ~ComponentFactory() = 0;
+    virtual Component* Create(Actor2D*, pugi::xml_node) = 0;
+};
+
 // class CarrierFactory : ComponentFactory {
 // protected:
 // public:
@@ -47,18 +48,18 @@
 //     ~InfantryFactory();
 //     Infantry* Create(Actor*, pugi::xml_node);
 // };
-//
-// class PlayerFactory : ComponentFactory {
-// protected:
-//     InputDevice* input_device_;
-//
-// public:
-//     PlayerFactory(InputDevice*);
-//     ~PlayerFactory();
-//     Player* Create(Actor*, pugi::xml_node);
-// };
-//
-// /*
+
+class PlayerFactory : public ComponentFactory {
+protected:
+    InputDevice* input_device_;
+
+public:
+    PlayerFactory(InputDevice*);
+    ~PlayerFactory();
+    Player* Create(Actor2D*, pugi::xml_node);
+};
+
+
 // class RigidCircleFactory : ComponentFactory {
 // protected:
 //     b2World* world_;
@@ -68,45 +69,45 @@
 //     ~RigidCircleFactory();
 //     RigidCircle* Create(Actor*, pugi::xml_node);
 // };
-// */
-//
-// class RigidRectangleFactory : ComponentFactory {
+
+
+class RigidRectangleFactory : public ComponentFactory {
+protected:
+    b2World* world_;
+
+public:
+    RigidRectangleFactory(b2World*);
+    ~RigidRectangleFactory();
+    RigidRectangle* Create(Actor2D*, pugi::xml_node);
+};
+
+class SpriteFactory : public ComponentFactory {
+protected:
+    SDLRenderer* renderer_;
+    ArtAssetLibrary* art_library_;
+
+public:
+    SpriteFactory(SDLRenderer*, ArtAssetLibrary*);
+    ~SpriteFactory();
+    Sprite* Create(Actor2D*, pugi::xml_node);
+};
+
+class AnimationFactory : public ComponentFactory {
+protected:
+
+
+public:
+    AnimationFactory();
+    ~AnimationFactory();
+    Animation* Create(Actor2D*, pugi::xml_node);
+};
+
+// class GlDrawableFactory : ComponentFactory {
 // protected:
-//     b2World* world_;
+//
 //
 // public:
-//     RigidRectangleFactory(b2World*);
-//     ~RigidRectangleFactory();
-//     RigidRectangle* Create(Actor*, pugi::xml_node);
+//     GlDrawableFactory();
+//     ~GlDrawableFactory();
+//     GlDrawable* Create(Actor3D*, pugi::xml_node);
 // };
-//
-// class SpriteFactory : ComponentFactory {
-// protected:
-//     Renderer* renderer_;
-//     ArtAssetLibrary* art_library_;
-//
-// public:
-//     SpriteFactory(Renderer*, ArtAssetLibrary*);
-//     ~SpriteFactory();
-//     Sprite* Create(Actor*, pugi::xml_node);
-// };
-//
-// class AnimationFactory : ComponentFactory {
-// protected:
-//
-//
-// public:
-//     AnimationFactory();
-//     ~AnimationFactory();
-//     Animation* Create(Actor*, pugi::xml_node);
-// };
-//
-// // class GlDrawableFactory : ComponentFactory {
-// // protected:
-// //
-// //
-// // public:
-// //     GlDrawableFactory();
-// //     ~GlDrawableFactory();
-// //     GlDrawable* Create(Actor*, pugi::xml_node);
-// // };
--- include/render/sdl/sprite.h
+++ include/render/sdl/sprite.h
@@ -1,23 +1,25 @@
-// #pragma once
-//
-// #include <memory>
-//
-// #include "virtual/component.h"
-// #include "render/sdl/sdl_renderer.h"
-// #include "render/sdl/sdl_texture.h"
-//
-// class SDLRenderer;
-//
-// class Sprite : public Component {
-// protected:
-//     SDLTexture* texture_;
-//     SDLRenderer* renderer_;
-//     SDL_Rect sprite_clip_;
-//
-// public:
-//     Sprite(Actor*);
-//     ~Sprite();
-//     void Initialize(Renderer*, SDLTexture*);
-//     void Update(std::shared_ptr<void> delta_time);
-//     void Draw();
-// };
+#pragma once
+
+#include <memory>
+
+#include "virtual/component.h"
+#include "render/sdl/actor_2d.h"
+#include "render/sdl/sdl_renderer.h"
+#include "render/sdl/sdl_texture.h"
+
+class SDLRenderer;
+
+class Sprite : public Component {
+protected:
+    Actor2D* owner_;
+    SDLTexture* texture_;
+    SDLRenderer* renderer_;
+    SDL_Rect sprite_clip_;
+
+public:
+    Sprite(Actor2D*);
+    ~Sprite();
+    void Initialize(SDLRenderer*, SDLTexture*);
+    void Update(std::shared_ptr<void> delta_time);
+    void Draw();
+};
